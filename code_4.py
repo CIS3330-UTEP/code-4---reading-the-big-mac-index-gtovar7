62242558afe3ed87638e5d5660c83804328cfe63
gtovar7 <gtovar7@miners.utep.edu>
2023-09-29 18:59:15 -0600

    Update code_4.py

--- code_4.py
+++ code_4.py
@@ -24,14 +24,14 @@ def get_the_cheapest_big_mac_price_by_year(year):
     cheap_year = df.query(query_3)
     cheap_min_idx = cheap_year['dollar_price'].idxmin()
     cheap_price = (round(cheap_year['dollar_price'].min(),2))
-    return f"{cheap_year.loc[cheap_min_idx]['name']}({cheap_year.loc[cheap_min_idx]['iso_a3']}): {cheap_price}"
+    return f"{cheap_year.loc[cheap_min_idx]['name']}({cheap_year.loc[cheap_min_idx]['iso_a3']}): ${cheap_price}"
 
 def get_the_most_expensive_big_mac_price_by_year(year):
     query_4 = f"(date >= '{year}-01-01' and date <= '{year}-12-31')"
     expensive_year = df.query(query_4)
     exp_price = (round(expensive_year['dollar_price'].max(),2))
     exp_min_idx = expensive_year['dollar_price'].idxmax()
-    return f"{expensive_year.loc[exp_min_idx]['name']}({expensive_year.loc[exp_min_idx]['iso_a3']}): {exp_price}"
+    return f"{expensive_year.loc[exp_min_idx]['name']}({expensive_year.loc[exp_min_idx]['iso_a3']}): ${exp_price}"
 
 if __name__ == "__main__":
     result_a = get_big_mac_price_by_year(2012,'arg')
